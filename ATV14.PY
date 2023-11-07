10df6e1f4507317a6e0bf7c4295343be36be7b41
yurezin <vitorgithub887@gmail.com>
2023-11-07 11:51:33 -0300

    as45

---
+++ ATV14.PY
@@ -0,0 +1,7 @@
+nt=float(input('sua primeira nota carinha:'))
+nt2=float(input('sua primeira nota carinha:'))
+md=(nt+nt2)/2
+if md>=6:
+    print('vc foi aprovado carinha')
+else:
+    print('vc foi reprovado')
---
+++ ATV27.PY
@@ -0,0 +1,36 @@
+carne = input("Digite F para filé duplo, A para alcatra e P para picanha: ")
+peso = float(input("Digite quantos quilos dessa carne vai comprar: "))
+pagamento = input("Dinheiro ou cartão tabajara (5% de desconto)? D ou C: ")
+preco_total = 0
+
+print("\nHipermercado Tabajara\nCupom fiscal\n")
+if carne == "F" or carne == "f":
+    print("Item: Filé Duplo")
+    if peso > 5:
+        preco_total = peso * 5.8
+    else:
+        preco_total = peso * 4.9
+elif carne == "A" or carne == "a":
+    print("Item: Alcatra")
+    if peso > 5:
+        preco_total = peso * 6.8
+    else:
+        preco_total = peso * 5.9
+elif carne == "P" or carne == "p":
+    print("Item: Picanha")
+    if peso > 5:
+        preco_total = peso * 7.8
+    else:
+        preco_total = peso * 6.9
+print(f"Quantidade: {peso:.2f}Kg")
+print(f"Preço total: R${preco_total:.2f}")
+if pagamento == "D" or pagamento == "d":
+    print("Tipo de pagamento: Dinheiro")
+    desconto = 0
+    print(f"Desconto: R${desconto:.2f}")
+    print(f"Valor a pagar: R${(preco_total - desconto):.2f}")
+elif pagamento == "C" or pagamento == "c":
+    print("Tipo de pagamento: Cartão Tabajara")
+    desconto = preco_total * 5 / 100
+    print(f"Desconto: R${desconto:.2f}")
+    print(f"Valor a pagar: R${(preco_total - desconto):.2f}")
